7fa9cb720cd5e1c9060686e52ab6e11a50874ea3
Andrius <andrius.pranciliauskas@gmail.com>
2019-06-16 16:50:34 +0300

    display diagnostic messages

--- packages/angularjs-language-service/src/features/getSemanticDiagnostics.ts
+++ packages/angularjs-language-service/src/features/getSemanticDiagnostics.ts
@@ -1,9 +1,25 @@
 import { Context } from '../types/context';
+import ts = require('typescript/lib/tsserverlibrary');
+import { PLUGIN_ID } from '../core/constants';
 
 export function getSemanticDiagnostics(
     context: Context,
     fileName: string
 ): ts.Diagnostic[] {
 
-    return [];
+    const result: ts.Diagnostic[] = [];
+    if (context.templateAst && context.templateAst.errors) {
+        for (const error of context.templateAst.errors) {
+            result.push({
+                file: undefined,
+                start: error.start,
+                length: error.end - error.start,
+                messageText: error.message,
+                category: ts.DiagnosticCategory.Error,
+                code: 0,
+                source: PLUGIN_ID,
+            });
+        }
+    }
+    return result;
 }
